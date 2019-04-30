77c9e9366fee94534c35cf8eaf2ef3a9035687c1
Camerone Stoney <Cameronestoney@yahoo.com>
2019-04-30 11:16:38 -0700

    varriables needed defined

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,20 @@
 // variables needed 
-var crystalOne= 0;
-var crystalTwo = 0;
-var crystalThree = 0;
+var crystalOne= 10;
+var crystalTwo = 3;
+var crystalThree = 1;
+var crystalFour = 7;
 var userNum = 0;
 var compNum = function(){
     Math.floor(Math.random() * ((120-19)+1) + 19);
 }
+
+// variables for where text goes
+var pointsText = document.getElementById("points");
+var winsText = document.getElementById("wins");
+var lossestext = document.getElementById("losses");
+
+// javascript to be run
+
+$("#crys-1").on("click",function(){
+
+});
