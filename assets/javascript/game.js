36a11bf0c642943f24dc3755e09d7a33f870ea41
Camerone Stoney <Cameronestoney@yahoo.com>
2019-04-30 10:55:15 -0700

    js started

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// variables needed 
+var crystalOne= 0;
+var crystalTwo = 0;
+var crystalThree = 0;
+var userNum = 0;
+var compNum = function(){
+    Math.floor(Math.random() * ((120-19)+1) + 19);
+}
