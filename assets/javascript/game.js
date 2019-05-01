ad145b2dba43e6c38fa5f1373119d9b2ac533266
Camerone Stoney <Cameronestoney@yahoo.com>
2019-05-01 15:40:01 -0700

    random functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,41 @@
-// variables needed 
-var crystalOne= 10;
-var crystalTwo = 3;
-var crystalThree = 1;
-var crystalFour = 7;
-var userNum = 0;
-var compNum = function(){
-    Math.floor(Math.random() * ((120-19)+1) + 19);
-}
 
-// variables for where text goes
-var pointsText = document.getElementById("points");
-var winsText = document.getElementById("wins");
-var lossestext = document.getElementById("losses");
 
-// javascript to be run
 
-$("#crys-1").on("click",function(){
+    // variables needed 
+    var crystalOne = Math.floor(Math.random()*12)+1;
+    var crystalTwo = Math.floor(Math.random()*12)+1;
+    var crystalThree = Math.floor(Math.random()*12)+1;
+    var crystalFour = Math.floor(Math.random()*12)+1;
+    var pointsBox = 0;
+    $('#points').text(pointsBox);
+    var userNum = 0;
+    var wins = 0;
+    var loses = 0;
 
+
+    var compNum = function () {
+        Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    }
+    // variables for where text goes
+
+    // do I need to define these?
+    var pointsText = document.getElementById("points");
+    var winsText = document.getElementById("wins");
+    var lossestext = document.getElementById("losses");
+
+    // javascript to be run
+    $(document).ready(function () {
+
+    console.log("ready!");
+
+    if (userNum < compNum) {
+
+        // function for crystal one 10 points
+        $("#crys-1").on("click", function () {
+            console.log("crystal1 clicked");
+            pointsBox += 10;
+           
+
+        });
+    }
 });
