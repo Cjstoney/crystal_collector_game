e91749cec9ac96113faed2820072ba1858b90363
Camerone Stoney <Cameronestoney@yahoo.com>
2019-05-02 15:24:55 -0700

    js changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,76 @@
 
 
     // variables needed 
-    var crystalOne = Math.floor(Math.random()*12)+1;
-    var crystalTwo = Math.floor(Math.random()*12)+1;
-    var crystalThree = Math.floor(Math.random()*12)+1;
-    var crystalFour = Math.floor(Math.random()*12)+1;
+    var crystalOne = crys1();
+    var crystalTwo = crys2();
+    // var crystalThree = crys3();
+    // var crystalFour = crys4();
     var pointsBox = 0;
-    $('#points').text(pointsBox);
-    var userNum = 0;
+    var targetNum = compNum();
+    var userNum =[];
+    var currentScore= sumArray();
     var wins = 0;
     var loses = 0;
 
-
-    var compNum = function () {
-        Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    // creating random number for the computer
+    function compNum() {
+        return Math.floor(Math.random() * ((120 - 19) + 1) + 19);
     }
-    // variables for where text goes
 
-    // do I need to define these?
-    var pointsText = document.getElementById("points");
-    var winsText = document.getElementById("wins");
-    var lossestext = document.getElementById("losses");
+    // function to get the sum of the userNum array
+    function sumArray(userNum) {
+        var sum = 0;
 
+        // keeps saying userNum is undefined
+        
+        for (i = 0; i < userNum.length; i++) {
+            sum = sum + userNum[i];
+        }
+        return sum;
+    }
+  
+    // functions for each crystal
+    function crys1() { 
+        return Math.floor(Math.random()*12)+1;
+    }
+    function crys2() { 
+        return Math.floor(Math.random()*7)+1;
+    }
+    // function crys3() { 
+    //     return Math.floor(Math.random()*11)+1;
+    // }
+    // function crys4() { 
+    //     return Math.floor(Math.random()*9)+1;
+    // }
+    // function pointsTotal(){
+
+    // }
+    
     // javascript to be run
     $(document).ready(function () {
 
     console.log("ready!");
+    console.log(targetNum);
+   
+    if (currentScore < targetNum) {
 
-    if (userNum < compNum) {
+console.log();
+        
+            // function for crystal one 
+            $("#crys-1").on("click", function () {
+                userNum.push(crystalOne);
+                console.log(userNum.reduce(reducer));
+                $("#points").text(sum);
+                
+            });
 
-        // function for crystal one 10 points
-        $("#crys-1").on("click", function () {
-            console.log("crystal1 clicked");
-            pointsBox += 10;
-           
 
-        });
+        
+    }else if(currentScore === targetNum){
+
+    }else{
+
     }
+    
 });
+
